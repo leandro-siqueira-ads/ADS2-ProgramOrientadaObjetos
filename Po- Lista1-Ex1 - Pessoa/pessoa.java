3d9b9d8b9cee9d37395c9d9f4c224214758519e4
Leandro Vinicius <112980662+leandro-siqueira-ads@users.noreply.github.com>
2023-03-29 19:28:56 -0300

    Update pessoa.java

--- Po- Lista1-Ex1 - Pessoa/pessoa.java
+++ Po- Lista1-Ex1 - Pessoa/pessoa.java
@@ -1,50 +1,46 @@
-package pessoa;
+package exercicio1po;
 
+public class Pessoa {
 	
-	public class pessoa {
-	    private String nome;
-	    private int numContribuinte;
-	    private int idade;
-	    
-	    public pessoa(String nome, int numContribuinte, int idade) {
-	        this.nome = nome;
-	        this.numContribuinte = numContribuinte;
-	        this.idade = idade;
-	    }
-	    
-	    public pessoa(String nome) {
-	        this.nome = nome;
-	        this.numContribuinte = 0;
-	        this.idade = 0;
-	    }
-	    
-	    public pessoa() {
-	        this.nome = "";
-	        this.numContribuinte = 0;
-	        this.idade = 0;
-	    }
-	    
-	    public String getNome() {
-	        return nome;
-	    }
-	    
-	    public void setNome(String nome) {
-	        this.nome = nome;
-	    }
-	    
-	    public int getNumContribuinte() {
-	        return numContribuinte;
-	    }
-	    
-	    public void setNumContribuinte(int numContribuinte) {
-	        this.numContribuinte = numContribuinte;
-	    }
-	    
-	    public int getIdade() {
-	        return idade;
-	    }
-	    
-	    public void setIdade(int idade) {
-	        this.idade = idade;
-	    }
+	String nome, nada;
+	int numeroContrib, idade;
+	
+	
+	public String getNome() {
+		return nome;
 	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getNumeroContrib() {
+		return numeroContrib;
+	}
+	public void setNumeroContrib(int numeroContrib) {
+		this.numeroContrib = numeroContrib;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
+	public Pessoa(String nome, int numeroContrib, int idade) {
+		this.nome = nome;
+		this.numeroContrib = numeroContrib;
+		this.idade = idade;
+		
+	}
+	
+	public Pessoa(String nome) {
+		
+		this.nome = nome;
+		
+	}
+	
+	public Pessoa() {
+		
+	}
+	
+}
